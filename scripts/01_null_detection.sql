f06f8aab202f9fb7d6b3b1a5c8abbfe69722782e
pegujosi <pegujosi@outlook.com>
2026-04-29 14:01:27 -0600

    Update on SQL file

--- scripts/01_null_detection.sql
+++ scripts/01_null_detection.sql
@@ -1,20 +1,20 @@
---query shows null quantity on sales column
+--query shows string null quantity on sales column
 SELECT COUNT(*) AS nulos
 FROM fact_ventas
 WHERE venta_bruta IS NULL;
 
---query shows null quantity per column (no primary key)
+--query shows string null quantity per column (no primary key)
 SELECT
 COUNT(*) FILTER (WHERE fecha IS NULL) AS nulos_fecha,
 COUNT(*) FILTER (WHERE unidades IS NULL) AS nulos_unidades,
 COUNT(*) FILTER (WHERE venta_bruta IS NULL) AS nulos_venta
 FROM fact_ventas;
 
---query shows total null quantity per column 
+--query shows total string null quantity per column 
 SELECT COUNT(*) - COUNT(*) FILTER (WHERE venta_bruta IS NOT NULL) AS nulos
 FROM fact_ventas;
 
---query shows total null quantity on sales column
+--query shows total string null quantity on sales column
 SELECT COUNT(*) - COUNT(venta_bruta) AS nulos
 FROM fact_ventas;
 
